4d49e6d6a194f2034b22d6f01df79844021494e7
Omar Baiazid <Omar.W.Baiazid@gmail.com>
2024-12-06 17:11:31 +0300

    Update and rename list sort to list sort.py

--- list sort.py
+++ list sort.py
@@ -1,4 +1,4 @@
-One of the simple challenges I faced was the gravity test where you are given a matrix and you have to consider each number as a cube and you have to move the big cube to the end and the small cube to the beginning. The explanation was more complicated than the solution, but with one function the question was solved.
+# One of the simple challenges I faced was the gravity test where you are given a matrix and you have to consider each number as a cube and you have to move the big cube to the end and the small cube to the beginning. The explanation was more complicated than the solution, but with one function the question was solved.
 
 
 from typing import List
@@ -8,7 +8,7 @@ def gravity_flip(columns: List[int]) -> List[int]:
 
 print(gravity_flip(columns = [3, 2, 1, 2]))
 -----------------------------------------------------------------------------------------
-Original solution without function
+# Original solution without function
 
 
 from typing import List
